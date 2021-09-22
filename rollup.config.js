0861e34318e7cb2f7e5cc19e91512e7ce9241ce8
Harvey Ramer <hramer@ducks.org>
2021-09-22 12:13:52 -0500

    Config changes to support Jest tests.

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,38 @@
 import typescript from 'rollup-plugin-typescript2'
 import commonjs from '@rollup/plugin-commonjs';
 import postcss from "rollup-plugin-postcss";
-import pkg from './package.json'
+import inject from '@rollup/plugin-inject';
+import pkg from './package.json';
 
-const rollupConfig = {
-  input: './src/react-library/index.tsx',
-  "output": {
-    "file": "lib/index.js",
-    "format": "esm",
-    "sourcemap": true
-  },
-  external: [
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {}),
-  ],
-  plugins: [
-    typescript({
-      tsconfig: "tsconfig.json",
-      useTsconfigDeclarationDir: true,
-    }),
-    commonjs(),
-    postcss({
-      extensions: ['.css']
-    })
-  ],
-};
+let config;
+if (process.env.NODE_ENV === 'test') {
+  config = {
+    plugins: [inject({ React: 'react' })],
+  }
+} else {
+  config = {
+    input: './src/react-library/index.tsx',
+    "output": {
+      "file": "lib/index.js",
+      "format": "esm",
+      "sourcemap": true
+    },
+    external: [
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {}),
+    ],
+    plugins: [
+      typescript({
+        tsconfig: "tsconfig.json",
+        useTsconfigDeclarationDir: true,
+      }),
+      commonjs(),
+      postcss({
+        extensions: ['.css']
+      }),
+      inject({ React: 'react' }),
+    ],
+  };
+}
 
-export default rollupConfig;
+export default config;
